98a24359ea690099ea6a3bce3f699bf33b9c2e3d
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-03-30 10:10:23 +0530

    first commit

---
+++ src/test/java/com/github/springcicdgithubactions/SpringCicdGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springcicdgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdGithubactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
